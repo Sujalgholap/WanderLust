47eb8c7bc812a5860b1a18373d7279a96f49dcd9
Sujal Gholap <sujalgholap1611@gmail.com>
2024-01-24 17:48:02 +0530

    Authorization for _reviews

--- routes/listing.js
+++ routes/listing.js
@@ -19,9 +19,9 @@ router.get("/", wrapAsync(async (req,res)=>{
   });
 
 //Show Route
-  router.get("/:id",isLoggedIn, wrapAsync(async (req,res)=>{
+  router.get("/:id", wrapAsync(async (req,res)=>{
     let {id} = req.params;
-    const listing = await Listing.findById(id).populate("reviews").populate("owner");
+    const listing = await Listing.findById(id).populate({path :"reviews", populate: { path : "author",}}).populate("owner");
     if(!listing){
       req.flash("error", "Listing you requested for does not exist");
       res.redirect("/listings");
