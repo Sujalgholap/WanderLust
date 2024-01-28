836d60f87309f818a310cf3effb0af34073d111e
Sujal Gholap <sujalgholap1611@gmail.com>
2024-01-28 19:49:26 +0530

    Map Popup

--- public/js/map.js
+++ public/js/map.js
@@ -1,11 +1,16 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
-container: 'map', // container ID
-center: coordinates, // starting position [lng, lat]
-zoom: 8 // starting zoom
+  container: "map", // container ID
+  center: listing.geometry.coordinates, // starting position [lng, lat]
+  zoom: 8, // starting zoom
 });
 
 // Create a default Marker and add it to the map.
-const marker1 = new mapboxgl.Marker({ color : "red" })
-.setLngLat(coordinates)
-.addTo(map);
+const marker1 = new mapboxgl.Marker({ color: "red" })
+.setLngLat(listing.geometry.coordinates)
+  .setPopup(
+    new mapboxgl.Popup({ offset: 25 }).setHTML(
+      `<h4>${listing.location}</h4><p>Exact location will be provided after booking</p>`
+    )
+  )
+  .addTo(map);
